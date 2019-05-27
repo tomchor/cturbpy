4a9e0f3cc3d259232f705103fefc5ac1c7aef4f6
tomchor <tomaschor@gmail.com>
2019-05-27 15:33:17 -0700

    working version

--- comp_rbc.py
+++ comp_rbc.py
@@ -44,7 +44,7 @@ print(f"Ratio: {ratio}\nΔz = {lz/nz}\nTotal size = {nz*nx} points\n")
 
 epsilon = 0.8
 Pr = 1.0
-Ra = 1e12
+Ra = 1e3
 
 dt_0 = 1e-10/scale
 
@@ -55,7 +55,8 @@ z_basis = de.Chebyshev('z', nz, interval=(-Lz/2, Lz/2), dealias=3/2)
 domain = de.Domain([x_basis, z_basis], grid_dtype=np.float64)
 
 # 2D Boussinesq hydrodynamics
-problem = de.IVP(domain, variables=['p','b','u','w','bz','uz','wz'], time='t')
+#problem = de.IVP(domain, variables=['p','b','u','w','bz','uz','wz'], time='t')
+problem = de.IVP(domain, variables=['p','u','w','ρ','uz','wz','ρz'], time='t')
 #problem.meta['p','b','w','bz','wz']['x']['parity'] = 1
 #problem.meta['u','uz']['x']['parity'] = -1
 problem.parameters['P'] = Pr
@@ -63,17 +64,17 @@ problem.parameters['R'] = Ra
 problem.parameters['F'] = F = 1
 
 problem.add_equation("dx(u) + wz = 0")
-problem.add_equation("dt(b) - (dx(dx(b)) + dz(bz))                   = - u*dx(b) - w*bz")
+problem.add_equation("dt(ρ) - (dx(dx(ρ)) + dz(ρz))                   = - u*dx(ρ) - w*ρz")
 problem.add_equation("dt(u) - P*(dx(dx(u)) + dz(uz)) + dx(p)         = - u*dx(u) - w*uz")
-problem.add_equation("dt(w) - P*(dx(dx(w)) + dz(wz)) + dz(p) - R*P*b = - u*dx(w) - w*wz")
-problem.add_equation("bz - dz(b) = 0")
+problem.add_equation("dt(w) - P*(dx(dx(w)) + dz(wz)) + dz(p) + R*P*ρ = - u*dx(w) - w*wz")
+problem.add_equation("ρz - dz(ρ) = 0")
 problem.add_equation("uz - dz(u) = 0")
 problem.add_equation("wz - dz(w) = 0")
 
-problem.add_bc("left(b) = -left(F*z)")
+problem.add_bc("left(ρ) = -left(F*z)")
 problem.add_bc("left(u) = 0")
 problem.add_bc("left(w) = 0")
-problem.add_bc("right(b) = -right(F*z)")
+problem.add_bc("right(ρ) = -right(F*z)")
 problem.add_bc("right(u) = 0")
 problem.add_bc("right(w) = 0", condition="(nx != 0)")
 problem.add_bc("integ_z(p) = 0", condition="(nx == 0)")
@@ -84,8 +85,8 @@ logger.info('Solver built')
 
 # Initial conditions
 z = domain.grid(1)
-b = solver.state['b']
-bz = solver.state['bz']
+ρ = solver.state['ρ']
+ρz = solver.state['ρz']
 
 # Random perturbations, initialized globally for same results in parallel
 gshape = domain.dist.grid_layout.global_shape(scales=1)
@@ -96,8 +97,8 @@ noise = rand.standard_normal(gshape)[slices]
 # Linear background + perturbations damped at walls
 zb, zt = z_basis.interval
 pert =  1e-2 * noise * (zt - z) * (z - zb)
-b['g'] = -F*(z - pert)
-b.differentiate('z', out=bz)
+ρ['g'] = -F*(z - pert)
+ρ.differentiate('z', out=ρz)
 
 # Integration parameters
 solver.stop_sim_time = 5
@@ -107,7 +108,7 @@ solver.stop_iteration = np.inf
 # Analysis
 snap = solver.evaluator.add_file_handler('snapshots_ra', sim_dt=0.001, max_writes=10)
 snap.add_task("p", scales=1, name='p')
-snap.add_task("b", scales=1, name='b')
+snap.add_task("ρ", scales=1, name='ρ')
 snap.add_task("u", scales=1, name='u')
 snap.add_task("w", scales=1, name='w')
 
