a01200be5e223c9ec784fa09d53def3b1e4a5039
tomchor <tomaschor@gmail.com>
2019-05-27 19:21:46 -0700

    almost there

--- rb4.py
+++ rb4.py
@@ -26,7 +26,7 @@ z_basis = de.Chebyshev('z', 256, interval=(0, Lz), dealias=3/2)
 domain = de.Domain([x_basis, z_basis], grid_dtype=np.float64)
 
 # Compressible NS
-problem = de.IVP(domain, variables=['w','wz','u','uz','T','Tz','ρ','ρz'])
+problem = de.IVP(domain, variables=['w','wz','u','uz','T','Tz','ρ','ρz','P'])
 
 
 problem.parameters['g']  = g
@@ -37,32 +37,35 @@ problem.parameters['κ'] = Prandtl*ν
 problem.parameters['γ'] = γ
 
 
-problem.substitutions["t_xx"] = ("2/3*(2*dx(u) - wz)")
-problem.substitutions["t_xz"] =("dx(w) + uz")
-problem.substitutions["t_zz"] =("2/3*(2*wz - dx(u))")
+problem.substitutions["Πxx"] = "2*dx(u) - 2/3*(dx(u) + wz)"
+problem.substitutions["Πzz"] = "2*dz(w) - 2/3*(dx(u) + wz)"
+problem.substitutions["Πxz"] = "dx(w) + uz"
+problem.substitutions["Πzx"] = "dx(w) + uz"
 
 
 problem.add_equation("dz(u) - uz = 0")
 problem.add_equation("dz(w) - wz = 0")
 problem.add_equation("dz(T) - Tz = 0")
 problem.add_equation("dz(ρ) - ρz = 0")
+problem.add_equation("P = ρ*T")
 
 #problem.add_equation("dt(ρ) + ρ*wz + ρ*dx(u) = -w*dz(ρ) - u*dx(ρ)")
 #problem.add_equation("dt(u) + dx(T) - ν*dx(t_xx) - ν*dz(t_xz) = - w*dz(u) - u*dx(u) - T*dx(log_rho)   + ν*t_xx*dx(log_rho) + ν*t_xz*dz(log_rho)")
 #problem.add_equation("dt(w) + Tz   - ν*dz(t_zz) - ν*dx(t_xz) = - w*wz - u*dx(w) - T*dz(log_rho) + g + ν*t_zz*dz(log_rho) + ν*t_xz*dx(log_rho)")
 #problem.add_equation("dt(T) - 1/Cv*(κ*(dz(Tz) + dx(dx(T)))) = - w*Tz - u*dx(T) - (γ-1)*T*(wz + dx(u)) + 1/Cv*(κ*(Tz*dz(log_rho) + dx(T)*dx(log_rho)) + ν*t_zz*wz + ν*t_xx*dx(u) + ν*t_xz*uz + ν*t_xz*dx(w))")
-problem.add_equation("dt(ρ) = -dz(w*ρ) - dx(u*ρ)")
-problem.add_equation("dt(u) - ν*dx(t_xx) - ν*dz(t_xz)       = - w*dz(u) - u*dx(u)   + ν*t_xx*dx(ρ)/ρ + ν*t_xz*dz(ρ)/ρ")
-problem.add_equation("dt(w) - ν*dz(t_zz) - ν*dx(t_xz)       = - w*wz    - u*dx(w)   + ν*t_zz*dz(ρ)/ρ + ν*t_xz*dx(ρ)/ρ")
-problem.add_equation("dt(T) - 1/Cv*(κ*(dz(Tz) + dx(dx(T)))) = - w*Tz    - u*dx(T) - (γ-1)*T*(wz + dx(u)) + 1/Cv*(κ*(Tz*dz(ρ)/ρ + dx(T)*dx(ρ)/ρ) \
-                                                                                    + ν*t_zz*wz + ν*t_xx*dx(u) + ν*t_xz*uz + ν*t_xz*dx(w))")
+#problem.add_equation("dt(T) - 1/Cv*(κ*(dz(Tz) + dx(dx(T)))) = - w*Tz    - u*dx(T) - (γ-1)*T*(wz + dx(u)) + 1/Cv*(κ*(Tz*dz(ρ)/ρ + dx(T)*dx(ρ)/ρ) + ν*Πzz*wz + ν*Πxx*dx(u) + ν*Πxz*uz + ν*Πxz*dx(w))")
+problem.add_equation("dt(ρ)                                 = - dz(w*ρ) - dx(u*ρ)")
+problem.add_equation("dt(u) - ν*dx(Πxx) - ν*dz(Πxz)         = - u*dx(u) - w*dz(u)   + dx(P)     + ν*Πxx*dx(ρ)/ρ + ν*Πxz*dz(ρ)/ρ")
+problem.add_equation("dt(w) + ν*dz(Πzx) - ν*dx(Πzz)         = - u*dx(w) - w*dz(w)   + dz(P)     + ν*Πzx*dx(ρ)/ρ + ν*Πzz*dz(ρ)/ρ")
+problem.add_equation("dt(T) - κ*(dz(Tz) + dx(dx(T)))        = - u*dx(T) - w*dz(T)   + κ*(Tz*dz(ρ)/ρ + dx(T)*dx(ρ)/ρ) \
+                                                                                    + ν*Πzz*wz + ν*Πxx*dx(u) + ν*Πxz*uz + ν*Πxz*dx(w)")
 
 #problem.add_bc("right(dz(rho_1)) = 0")
 problem.add_bc("left(dz(ρ)) = 0")
-problem.add_bc("left(T) = 270.001")
-problem.add_bc("right(T) = 270.00")
-problem.add_bc("left(dz(u)) = 0")
-problem.add_bc("right(dz(u)) = 0")
+problem.add_bc("left(T) = 270.01")
+problem.add_bc("right(T) = 270.000")
+problem.add_bc("left(u) = 0")
+problem.add_bc("right(u) = 0")
 problem.add_bc("left(w) = 0")
 problem.add_bc("right(w) = 0")
 
@@ -78,7 +81,6 @@ z = domain.grid(1)
 ρ = solver.state['ρ']
 T = solver.state['T']
 Tz = solver.state['Tz']
-#rho_1 = solver.state['rho_1']
 
 solver.evaluator.vars['Lx'] = Lx
 solver.evaluator.vars['Lz'] = Lz
@@ -87,12 +89,12 @@ solver.evaluator.vars['Lz'] = Lz
 
 tanh_width = Lz/20.0
 tanh_center = 0.5*Lz
-rho = 4.0
+rho = 1.2
 
 A_u = 1
 sigma = 0.2
 amp = -0.02
-ρ['g'] = np.log(rho)
+ρ['g'] = rho
 Press = 50 + rho*g*z
 T['g'] = Press/(rho*R)
 T.differentiate('z',out=Tz)
@@ -103,7 +105,7 @@ logger.info("T = {:g} -- {:g}".format(np.min(T['g']), np.max(T['g'])))
 
 
 # Initial timestep
-dt = 0.00125
+dt = 0.001
 
 # Integration parameters
 solver.stop_sim_time = tstop
@@ -115,7 +117,7 @@ snapshots = solver.evaluator.add_file_handler('snapshots', iter=1, max_writes=50
 snapshots.add_task('T')
 snapshots.add_task('w')
 snapshots.add_task('wz')
-snapshots.add_task('0.8*T*ρ', name='P')
+snapshots.add_task('T*ρ', name='P')
 snapshots.add_task('ρ', name='ρ')
 snapshots.add_task('u', name='u')
 # CFL
@@ -125,7 +127,6 @@ CFL.add_velocities(('u', 'w'))
 
 # Flow properties
 flow = flow_tools.GlobalFlowProperty(solver, cadence=10)
-flow.add_property("T", name='u_rms')
 
 # Main loop
 try:
@@ -136,7 +137,6 @@ try:
         dt = solver.step(dt)
         if (solver.iteration-1) % 1 == 0:
             logger.info('Iteration: %i, Time: %e, dt: %e' %(solver.iteration, solver.sim_time, dt))
-            logger.info('Max u)rms = %f' %flow.max('u_rms'))
 except:
     logger.error('Exception raised, triggering end of main loop.')
     raise
